212820c1f9c92945afe3c09b326f86f340e7eff7
Arnaud Alies <arnaud.alies@epitech.eu>
2016-01-13 18:01:40 +0100

    ok

--- src/map/pos.c
+++ src/map/pos.c
@@ -5,20 +5,35 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Wed Jan 13 15:58:21 2016 Arnaud Alies
-** Last update Wed Jan 13 16:09:45 2016 Arnaud Alies
+** Last update Wed Jan 13 17:29:03 2016 Arnaud Alies
 */
 
 #include "map.h"
 
-t_block	*get_map(t_map *map, int x, int y, int z)
+t_block	*map_get(t_map *map, t_pos *pos)
 {
   int	size;
-  int	pos;
+  int	x;
 
   size = (map->head).size;
-  pos = x + y * size + z * (size * size);
-  if (pos >= 0 && pos < (size * size * size))
-    return (&((map->blocks)[pos]));
+  x = pos->x + pos->y * size + pos->z * (size * size);
+  if (x >= 0 && x < (size * size * size))
+    return (&((map->blocks)[x]));
   return (NULL);
+}
+
+int	map_set(t_map *map, t_pos *pos, t_block block)
+{
+  int   size;
+  int   x;
+
+  size = (map->head).size;
+  x = pos->x + pos->y * size + pos->z * (size * size);
+  if (x >= 0 && x < (size * size * size))
+    {
+      ((map->blocks)[x]) = block;
+      return (0);
+    }
+  return (1);
 }
 
