efef328f0e82ee6782e803854eccf2a8887b3df9
alies_a <arnaud.alies@epitech.eu>
2016-01-26 10:09:37 +0100

    ok

--- src/mob/mob_loop.c
+++ src/mob/mob_loop.c
@@ -5,10 +5,20 @@
 ** Login   <alies_a@epitech.net>
 ** 
 ** Started on  Thu Jan 21 17:57:17 2016 alies_a
-** Last update Tue Jan 26 09:55:32 2016 alies_a
+** Last update Tue Jan 26 10:08:56 2016 alies_a
 */
 
 #include "doom.h"
+
+static void	mob_attack(t_data *data, t_mob *mob)
+{
+  t_pos		pos;
+
+  pos.x = (mob->pos).x;
+  pos.y = (mob->pos).y;
+  pos.z = (mob->pos).z + 1;
+  blow_at(data->map, &pos, 2);
+}
 
 void	mob_loop(t_data *data)
 {
@@ -23,6 +33,7 @@ void	mob_loop(t_data *data)
 	{
 	  ((mob[x]).pos).x -= (((mob[x]).pos).x - ((data->me).pos).x) / 100;
 	  ((mob[x]).pos).y -= (((mob[x]).pos).y - ((data->me).pos).y) / 100;
+	  mob_attack(data, &(mob[x]));
 	}
       x += 1;
     }
