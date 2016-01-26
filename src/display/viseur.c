12c8685c5269618148ab28f70dd5a878daf1d35b
josso_a <arthur.josso@epitech.eu>
2016-01-26 19:59:29 +0100

    viseur plus petit

--- src/display/viseur.c
+++ src/display/viseur.c
@@ -5,7 +5,7 @@
 ** Login oddou_f <frederic.oddou@epitech.eu>
 **
 ** Started on  Tue Jan 19 16:24:59 2016 Frederic ODDOU
-** Last update Tue Jan 19 16:24:59 2016 Frederic ODDOU
+** Last update Tue Jan 26 19:57:55 2016 Arthur Josso
 */
 
 #include "doom.h"
@@ -18,9 +18,9 @@ void			viseur(t_data *data)
 
   i = 0;
   color.full = 0xa5a5a5;
-  pos.x = (data->pix->clipable).clip_width / 2 - 7;
+  pos.x = (data->pix->clipable).clip_width / 2 - 4;
   pos.y = (data->pix->clipable).clip_height / 2;
-  while (i < 14)
+  while (i < 7)
     {
       pos.x += 1;
       tekpixel(data->pix, &pos, &color);
@@ -28,8 +28,8 @@ void			viseur(t_data *data)
     }
   i = 0;
   pos.x = (data->pix->clipable).clip_width / 2;
-  pos.y = (data->pix->clipable).clip_height / 2 - 7;
-  while (i < 14)
+  pos.y = (data->pix->clipable).clip_height / 2 - 4;
+  while (i < 7)
     {
       pos.y += 1;
       tekpixel(data->pix, &pos, &color);
